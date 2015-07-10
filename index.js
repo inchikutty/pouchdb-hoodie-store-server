d060bdd001095e9ca3292f3b1a11ac86323e4f0a
inchikutty <inchikutty13@gmail.com>
2015-07-11 01:21:57 +0530

    Serve all static assets from current folder
    This commit enable app to serve all static assets from current folder, not
    just index.html

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 	module.exports = function (options, callback) {
 		var app = express();
 
-		// serves main page
-		app.get('/', function(req, res) {
-			res.sendFile('index.html', { root: options.path });
-		});
+		//serve any static asset
+		app.use(express.static('files'));
+		app.use(express.static( options.path));
 
 		app.listen(options.port, callback);
 	}
